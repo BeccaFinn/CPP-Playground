50c982dec44037bed141fe53b4a805f91d07ad8c
Rebecca Finn <rebeccafinn283@gmail.com>
2022-12-28 23:54:36 -0500

    First Chapter Project
    First commit for ch. 1 project

---
+++ Malik - Exercises/Chapter One/src/Chapter One.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Chapter.cpp
+// Author      : Rebecca Finn
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
